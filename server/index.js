3e516d3a7b2bd939a0d89c535d9d257614628af6
Gideon Baik <gideonbaik@gmail.com>
2018-02-19 00:47:21 -0800

    Add SQL query to grab customer_id customer_first_name category_id category_name number_purchased

--- server/index.js
+++ server/index.js
@@ -1,13 +1,48 @@
 const express = require('express');
 const path = require('path');
-const app = express();
+const model = require('../db/models');
 
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('*', function (req, res) {
-  res.end('Hello World');
+
+  model.Order.where({id: 1}).fetch({withRelated: ['customer', 'product']})
+    .then(data => {
+      let customer = data.related('customer').toJSON();
+      let customerId = customer.id;
+      let customerFirstName = customer.first_name;
+      let orderNumber = customer.number;
+      let product = data.related('product').toJSON()[0];
+      let output = [customerId, customerFirstName];
+
+      model.Product.where({id: product.product_id}).fetch({withRelated: ['category']})
+      .then(data => {
+          let category = data.related('category').toJSON()[0];
+          let categoryId = category.category_id;
+
+          output.push(categoryId);
+      
+          model.Category.where({id: categoryId}).fetch({withRelated: ['product']})
+            .then(data => {
+              let categoryName = data.toJSON().name;
+              
+              output.push(categoryName);
+              output.push(orderNumber);
+              
+              res.send(output)
+            })
+            .catch(err => {
+              res.status(500).send(err);
+            });
+        })
+
+
+    })
+
 });
 
 app.listen(port, _ => {
   console.log(`Server connected to port number: ${port}`);
 }); 
+
