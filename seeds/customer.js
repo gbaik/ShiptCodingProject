7e9d7be3184c4168abcfbd8b570e13e86dade8a8
Gideon Baik <gideonbaik@gmail.com>
2018-02-19 18:00:00 -0800

    Fix seed file to seed number in order instead of customer

--- seeds/customer.js
+++ seeds/customer.js
@@ -2,11 +2,11 @@ exports.seed = function(knex, Promise) {
   return knex('customer').del()
     .then(function () {
       return knex('customer').insert([
-        {first_name: 'Cecelia', number:'H1jCZBIDG'},
-        {first_name: 'Rosie', number: 'BJxsR-HLvz'},
-        {first_name: 'Beth', number: 'HJxMzHIDM'},
-        {first_name: 'Ben', number: 'S1xlzfBLDz'},
-        {first_name: 'Marcus', number: 'S1WgMMSIwM'}        
+        {first_name: 'Cecelia'},
+        {first_name: 'Rosie'},
+        {first_name: 'Beth'},
+        {first_name: 'Ben'},
+        {first_name: 'Marcus'}        
       ]);
     });
 };
--- seeds/order.js
+++ seeds/order.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('order').del()
     .then(function () {
       return knex('order').insert([
-        {customer_id: '11', status_id: '7'},
-        {customer_id: '13', status_id: '8'},
-        {customer_id: '14', status_id: '9'}
+        {customer_id: '11', status_id: '7', number:'H1jCZBIDG'},
+        {customer_id: '13', status_id: '8', number: 'BJxsR-HLvz'},
+        {customer_id: '14', status_id: '9', number: 'S1xlzfBLDz'}
       ]);
     });
 };
